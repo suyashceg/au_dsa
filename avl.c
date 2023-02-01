2d03966b58215747cd84df7c1e3bca74eb8b5231
Suyash <63108010+suyashceg@users.noreply.github.com>
2023-02-01 13:04:47 +0530

    Add files via upload

---
+++ avl.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node 
+{
+    int data;
+    struct node *right;
+    struct node *left;
+    int height;
+};
+
+int getheight(struct node *root)
+{
+    
+}
